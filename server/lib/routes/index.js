f26dffc797268ee63a5a87040720c889290d4998
Sam Bateman <samlukebateman@gmail.com>
2019-04-12 10:24:46 +0100

    Server now hosts & opens the built react app when started

--- server/lib/routes/index.js
+++ server/lib/routes/index.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const express = require("express");
+const path = require("path");
 
 // Routing here
 const dataRoutes = require("./data");
 
 const router = express.Router();
+
+router.use("/",  express.static(path.join(__dirname, "../../../build/")));
+router.get("/", (req, res) => {
+    res.set("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, "../../../build/index.html"));
+});
 
 router.use("/data", dataRoutes);
 

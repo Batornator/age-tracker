11cd133db1ad1b45eb174425e24661c49371f05d
Sam Bateman <samlukebateman@gmail.com>
2019-07-04 21:35:19 +0100

    fix bug in csv download

--- server/lib/routes/csv/download.js
+++ server/lib/routes/csv/download.js
@@ -1,12 +1,32 @@
 "use strict";
 
 const path = require("path");
-
 const filePath = path.join(__dirname, "../../store/ageData.csv");
+
+const ageCalculationProgram = require("../../programs/ageCalculation");
+const ageDataStore = require("../../store/ageData");
+
+const calculateAges = (data) => {
+  return data.map((record) => ageCalculationProgram.calculateAge(record));
+};
 
 // GET:/csv/
 module.exports = (req, res) => {
 
-    res.status(200).download(filePath);
+  ageDataStore.getData((err, results) => {
+    if (err) {
+      return res.status(err.status).json({
+        errors: err.errors
+      });
+    }
+
+    ageDataStore.writeDataToFile(calculateAges(results), (err) => {
+      if (err) {
+        return cb(err);
+      }
+
+      res.status(200).download(filePath);
+    });
+  });
 
 };
